6b195cc4f154f5fa6e7e430c230e71e0bf915e6a
Baur Urazalinov <burazalinov@gmail.com>
2021-01-02 19:53:17 -0600

    Corrected.

--- run-perf-tests.sh
+++ run-perf-tests.sh
@@ -4,9 +4,6 @@
 # ./run-perf-tests.sh - will create an infrastructure with 1 master node and 1 worker node
 # ./run-perf-tests.sh 3 - will create an infrastructure with 1 master node and 3 worker nodes
 
-YELLOW=`tput setaf 3`
-RESET=`tput sgr0`
-
 if ["$1" == ""];
 then
   export WORKERS=1
