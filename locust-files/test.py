e1ec633f142f9b52701c63b7ea3b408add5acd1c
Baur Urazalinov <BurhanH@users.noreply.github.com>
2022-01-23 11:02:43 -0600

    Update test.py

--- locust-files/test.py
+++ locust-files/test.py
@@ -20,24 +20,44 @@ class QuickStart(HttpUser):
     @task
     def random_quote(self):
         # Trying to get random quote from the end point
-        self.client.get(
+        with self.client.get(
             url=API_ENDPOINT_PREFIX,
             headers=HEADERS,
-        )
+            catch_response=True
+        ) as response:
+            if response.status_code != 200:
+                response.failure("Failed to get random quote")
+            if response.text == '{}':
+                response.failure("Empty response")
+            if response.elapsed.total_seconds() > 0.5:
+                response.failure("Response took longer than 0.5 second")
 
     @task
     def first_quote(self):
         # Trying to get first quote from the end point
-        self.client.get(
+        with self.client.get(
             url=f'{API_ENDPOINT_PREFIX}/1',
             headers=HEADERS,
-        )
+        ) as response:
+            if response.status_code != 200:
+                response.failure("Failed to get first quote")
+            if response.text == '{}':
+                response.failure("Empty response")
+            if response.elapsed.total_seconds() > 0.5:
+                response.failure("Response took longer than 0.5 second")
 
     @task
     def any_quote(self):
         # Trying to get any quote (from 2 to 11) from the end point
         # Note! We exclude quote 1, this scenario covers in the previous task
-        self.client.get(
-            url=f'{API_ENDPOINT_PREFIX}/{SystemRandom().randint(2, 11)}',
+        quiote_id = SystemRandom().randint(2, 11)
+        with self.client.get(
+            url=f'{API_ENDPOINT_PREFIX}/{quiote_id}',
             headers=HEADERS,
-        )
+        ) as response:
+            if response.status_code != 200:
+                response.failure(f'Failed to get {quiote_id} quote')
+            if response.text == '{}':
+                response.failure("Empty response")
+            if response.elapsed.total_seconds() > 0.5:
+                response.failure("Response took longer than 0.5 second")
