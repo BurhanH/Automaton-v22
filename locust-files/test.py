4eb2b0aec9a682593947bdbfe11a6eea82cbfbe6
Baur Urazalinov <burazalinov@signifyhealth.com>
2022-01-23 11:29:39 -0600

    DRY and KISS

--- locust-files/test.py
+++ locust-files/test.py
@@ -2,8 +2,8 @@ from random import SystemRandom
 
 from locust import HttpUser, task
 
-# Target URL - api-flask-baur.herokuapp.com/
-API_ENDPOINT_PREFIX = 'api/v1/quotes'
+# Target URL - http://api-flask-baur.herokuapp.com
+API_ENDPOINT_PREFIX = '/api/v1/quotes'
 HEADERS = {'Content-type': 'application/json'}
 
 
@@ -17,47 +17,36 @@ class QuickStart(HttpUser):
         # Close client session, etc.
         pass
 
-    @task
-    def random_quote(self):
-        # Trying to get random quote from the end point
+    def get_quote(self, quote_id=None):
+        target_endpoint = API_ENDPOINT_PREFIX
+        if quote_id:
+            target_endpoint = f'{API_ENDPOINT_PREFIX}/{quote_id}'
+
         with self.client.get(
-            url=API_ENDPOINT_PREFIX,
+            url=target_endpoint,
             headers=HEADERS,
             catch_response=True
         ) as response:
             if response.status_code != 200:
-                response.failure("Failed to get random quote")
-            if response.text == '{}':
+                response.failure("Failed to get quote")
+            if response.text == "{}":
                 response.failure("Empty response")
             if response.elapsed.total_seconds() > 0.5:
                 response.failure("Response took longer than 0.5 second")
 
     @task
+    def random_quote(self):
+        # Trying to get random quote from the end point
+        self.get_quote()
+
+    @task
     def first_quote(self):
         # Trying to get first quote from the end point
-        with self.client.get(
-            url=f'{API_ENDPOINT_PREFIX}/1',
-            headers=HEADERS,
-        ) as response:
-            if response.status_code != 200:
-                response.failure("Failed to get first quote")
-            if response.text == '{}':
-                response.failure("Empty response")
-            if response.elapsed.total_seconds() > 0.5:
-                response.failure("Response took longer than 0.5 second")
+        self.get_quote(1)
 
     @task
     def any_quote(self):
         # Trying to get any quote (from 2 to 11) from the end point
         # Note! We exclude quote 1, this scenario covers in the previous task
         quiote_id = SystemRandom().randint(2, 11)
-        with self.client.get(
-            url=f'{API_ENDPOINT_PREFIX}/{quiote_id}',
-            headers=HEADERS,
-        ) as response:
-            if response.status_code != 200:
-                response.failure(f'Failed to get {quiote_id} quote')
-            if response.text == '{}':
-                response.failure("Empty response")
-            if response.elapsed.total_seconds() > 0.5:
-                response.failure("Response took longer than 0.5 second")
+        self.get_quote(quiote_id)
